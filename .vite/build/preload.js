c011232581fbfe82ed1a8468ee2f87058db04bb2
Johnny Sung <j796160836@gmail.com>
2026-02-03 17:03:37 +0800

    feat: add github actions

--- .vite/build/preload.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const { contextBridge, ipcRenderer } = require("electron");
-contextBridge.exposeInMainWorld("electronAPI", {
-  openFile: () => ipcRenderer.invoke("dialog:openFile"),
-  readFile: (path) => ipcRenderer.invoke("fs:readFile", path),
-  saveFile: (data) => ipcRenderer.invoke("fs:saveFile", data),
-  showMessageBox: (options) => ipcRenderer.invoke("dialog:messageBox", options)
-});
